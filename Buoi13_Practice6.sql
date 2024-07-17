0d67af08b684d0b7faab36d823199c1c45f8de60
congalvaro <168463441+congalvaro@users.noreply.github.com>
2024-07-17 12:39:56 +0700

    Update Buoi13_Practice6.sql

--- Buoi13_Practice6.sql
+++ Buoi13_Practice6.sql
@@ -5,3 +5,15 @@ GROUP BY company_id, title, description)
 SELECT count(company_id) AS duplicate_companies FROM duplicate_count
 WHERE dup_count > 1;
 --ex2: datalemur-highest-grossing.
+WITH ranked_list AS
+(
+SELECT 
+  category, 
+  product, 
+  SUM(spend) AS total_spend,
+  RANK() OVER(PARTITION BY category ORDER BY SUM(spend) DESC) AS rank_spend
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date)= 2022
+GROUP BY category, product)
+SELECT category, product, total_spend FROM ranked_list
+WHERE rank_spend <= 2;
